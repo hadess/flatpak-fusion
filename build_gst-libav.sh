3124ba3a5b4597ecfda561edcbbea325e62a067c
Bastien Nocera <hadess@hadess.net>
2016-05-03 17:57:20 +0200

    net.gstreamer.libav: Fix bundle generation
    It was trying to create an app bundle, and on the wrong branch.

--- build_gst-libav.sh
+++ build_gst-libav.sh
@@ -7,16 +7,18 @@
 FILE=`dirname $0`/net.gstreamer.libav.json
 
 APPID=`basename $FILE .json`
+NAME=org.freedesktop.Platform.GStreamer.libav
 SUBJECT="Nightly build of gst-libav, `date`"
+BRANCH=1.4
 
 echo ========== Building $APPID ================
 rm -rf runtime
 xdg-app-builder --ccache --repo=repo --build-only --subject="${SUBJECT}" ${EXPORT_ARGS-} runtime $FILE
 cat << EOF > runtime/metadata
 [Runtime]
-name=org.freedesktop.Platform.GStreamer.libav
-runtime=org.freedesktop.Platform/x86_64/1.4
-sdk=org.freedesktop.Sdk/x86_64/1.4
+name=$NAME
+runtime=org.freedesktop.Platform/x86_64/$BRANCH
+sdk=org.freedesktop.Sdk/x86_64/$BRANCH
 tags=nightly;
 EOF
 
@@ -25,7 +27,7 @@ rm -rf runtime/files/{bin,include,share} runtime/file/lib/gstreamer-1.0/*.la
 if [ ! -d repo ] ; then
     ostree  init --mode=archive-z2 --repo=repo
 fi
-ostree commit --repo=repo --owner-uid=0 --owner-gid=0 --no-xattrs --branch=runtime/org.freedesktop.Platform.GStreamer.libav/x86_64/1.4  --subject="${SUBJECT}" runtime
+ostree commit --repo=repo --owner-uid=0 --owner-gid=0 --no-xattrs --branch=runtime/org.freedesktop.Platform.GStreamer.libav/x86_64/$BRANCH --subject="${SUBJECT}" runtime
 xdg-app build-update-repo repo
 xdg-app build-finish runtime
-xdg-app build-bundle repo/ $APPID.x86_64.xdgapp $APPID master
+xdg-app build-bundle --runtime repo/ $APPID.x86_64.xdgapp $NAME $BRANCH
