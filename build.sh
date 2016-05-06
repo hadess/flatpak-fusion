8c4422941411855facd9c54a3cb1b12304b8c1ba
Bastien Nocera <hadess@hadess.net>
2016-05-07 01:49:20 +0200

    build: Fix outdated bundle being created
    In some cases, it would be possible for the repo to contain enough
    information to extract a bundle, even if the build didn't finish
    successfully.
    
    Fix that by only creating the bundle if the build succeeded.

--- build.sh
+++ build.sh
@@ -7,5 +7,5 @@ APPID=`basename $FILE .json`
 
 echo ========== Building $APPID ================
 rm -rf app
-xdg-app-builder --ccache --require-changes --repo=repo --subject="${APPID} ${VERSION}" ${EXPORT_ARGS-} app $FILE
+xdg-app-builder --ccache --require-changes --repo=repo --subject="${APPID} ${VERSION}" ${EXPORT_ARGS-} app $FILE && \
 xdg-app build-bundle repo/ $APPID.x86_64.xdgapp $APPID master
