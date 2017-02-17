8f5a2b1ad732054552e9d4a6338ef864eb0d84b7
Bastien Nocera <hadess@hadess.net>
2017-02-17 15:54:41 +0100

    build: Support building runtime using build.sh
    So we don't need a special script for runtimes.

--- build.sh
+++ build.sh
@@ -4,8 +4,14 @@ FILE=$1
 VERSION=$2
 
 APPID=`grep -Po '(?<="id": ")[^"]*' $FILE`
+if grep -q 'build-extension": true' $FILE ; then
+  RUNTIME_FLAG="--runtime"
+  TYPE="runtime"
+else
+  TYPE="app"
+fi
 
-echo ========== Building $APPID ================
+echo "========== Building $APPID ($TYPE) ================"
 rm -rf app
 flatpak-builder --ccache --require-changes --repo=repo --subject="${APPID} ${VERSION}" ${EXPORT_ARGS-} app $FILE && \
-flatpak build-bundle repo/ $APPID.x86_64.flatpak $APPID master
+flatpak build-bundle $RUNTIME_FLAG repo/ $APPID.x86_64.flatpak $APPID master
